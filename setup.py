b254ba99df5856030dfcb1d13b14c1c8a41108b9
Matt Yule-Bennett <matt@bennett.name>
2017-04-11 16:33:50 +0100

    Release 0.0.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='nameko-sentry',
-    version='0.0.4',
+    version='0.0.5',
     description='Nameko extension sends entrypoint exceptions to sentry',
     author='Matt Bennett',
     author_email='matt@bennett.name',
